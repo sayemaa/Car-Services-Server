2331796c7e358d0cf47fb288ab758bf466e37253
sayemaa <sayema990@gmail.com>
2022-04-30 15:15:05 +0600

    Nodemon added

---
+++ __server_deploy.js
@@ -0,0 +1,15 @@
+/**
+ * ----------------------------
+ *  ONE TIME for your computer
+ * ----------------------------
+ * 1. Create heroku account 
+ * 2. Verify email
+ * 3. Install heroku CLI
+ * 4. heroku login
+ * -------------------------------
+ * For each project ONE TIME
+ * -------------------------------
+ * 1. heroku create
+ * 2. Make sure to: git add . , git commit, git push
+ * 3. git push heroku main
+ */
